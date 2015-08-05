ae48b7e28255fc740f17234bce134dbb9a577cf4
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-08-05 18:24:00 +0100

    Use AWS CLI to retrieve information where possible.

--- create-instance-profile.sh
+++ create-instance-profile.sh
@@ -6,15 +6,16 @@ NAME=${1}
 PDP="file://$(pwd)/codedeploy_policy_document.json"
 RDP="file://$(pwd)/codedeploy_role_document.json"
 
-POLICY=$(aws iam create-policy --policy-name ${NAME}_policy --policy-document ${PDP})
-POLICY_ARN=$(echo $POLICY | sed -e 's/^.*\"Arn\"/\"Arn\"/g' -e 's/\"Arn\": \"\([^\"]*\)\".*$/\1/g')
+POLICY_ARN=$(aws iam create-policy --policy-name ${NAME}_policy --policy-document ${PDP} --query Policy.Arn --output text)
+echo "POLICY_ARN: $POLICY_ARN"
 
 ROLE=$(aws iam create-role --role-name ${NAME}_role --assume-role-policy-document ${RDP})
-ROLE_NAME=$(echo $ROLE | sed -e 's/^.*\"RoleName": \"\([^\"]*\)\".*$/\1/g')
+ROLE_NAME=$(aws iam get-role --role-name ${NAME}_role --query "Role.RoleName" --output text)
+echo "ROLE_NAME: $ROLE_NAME"
 
 PROFILE=$(aws iam create-instance-profile --instance-profile-name ${NAME})
-PROFILE_NAME=$(echo $PROFILE | sed -e 's/^.*\"InstanceProfileName": \"\([^\"]*\)\".*$/\1/g')
-PROFILE_ARN=$(echo $PROFILE | sed -e 's/^.*\"Arn": \"\([^\"]*\)\".*$/\1/g')
+PROFILE_NAME=$(aws iam get-instance-profile --instance-profile-name ${NAME} --query "InstanceProfile.InstanceProfileName" --output text)
+PROFILE_ARN=$(aws iam get-instance-profile --instance-profile-name ${NAME} --query "InstanceProfile.Arn" --output text)
 
 aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn $POLICY_ARN
 aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
