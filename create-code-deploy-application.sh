76d9d6ba7f7a149e3871e298929648fcd79bbd3a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-08-26 15:31:15 +0100

    remove instance profile from create-application code
    In order to have a CodeDeploy application, you need a Service Role ARN
    to add to your deployment group.  However, that Role doesn't need to be
    attached to an Instance Profile.  Moreover, we can hardcode the value of
    the Service Role ARN because it's the same each time.
    
    This also adds a script to create the code deploy service role.

--- create-code-deploy-application.sh
+++ create-code-deploy-application.sh
@@ -2,18 +2,9 @@
 
 APPLICATION_NAME=$1
 DEPLOYMENT_GROUP_NAME=$2
-INSTANCE_PROFILE_NAME=$3
-BUCKET_NAME=$4
+BUCKET_NAME=$3
 
-
-fetchInstanceProfile(){
-    CODE_DEPLOYER_PROFILE_ARN=$(aws iam get-instance-profile \
-                                --instance-profile-name $INSTANCE_PROFILE_NAME \
-                                --query InstanceProfile.Roles[0].Arn \
-                                --output text)
-
-    echo "$CODE_DEPLOYER_PROFILE_ARN"
-}
+CODE_DEPLOYER_ROLE_ARN=arn:aws:iam::022990953738:role/code-deploy-role
 
 
 createApplication(){
@@ -25,7 +16,6 @@ createApplication(){
 }
 
 createDeploymentGroup(){
-    CODE_DEPLOYER_PROFILE_ARN=$1
     INSTANCE_TAG_FILTERS="Key=Environment,Value=preview,Type=KEY_AND_VALUE"
 
     DEPLOYMENT_GROUP_ID=$(aws deploy create-deployment-group \
@@ -33,41 +23,36 @@ createDeploymentGroup(){
             --deployment-group-name $DEPLOYMENT_GROUP_NAME \
             --deployment-config-name CodeDeployDefault.AllAtOnce \
             --ec2-tag-filters ${INSTANCE_TAG_FILTERS} \
-            --service-role-arn ${CODE_DEPLOYER_PROFILE_ARN} \
+            --service-role-arn ${CODE_DEPLOYER_ROLE_ARN} \
             --output text)
 
     echo "$DEPLOYMENT_GROUP_ID"
 }
 
 createBucket(){
-    CODE_DEPLOYER_PROFILE_ARN=$1
-
     aws s3 mb s3://${BUCKET_NAME} --region=eu-west-1
 }
 
 
 usage() {
-    echo "Usage: $0 application-name deployment-group-name instance-propfile-name s3-bucket-name"
+    echo "Usage: $0 application-name deployment-group-name s3-bucket-name"
     echo
     echo "Creates an application with deployment group at code-deploy"
 }
 
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Wrong number of arguments"
     usage; exit
 fi
 
 runScript(){
-    CODE_DEPLOYER_PROFILE_ARN=$(fetchInstanceProfile)
-    echo "Retreived instance profile arn $CODE_DEPLOYER_PROFILE_ARN"
-
     APPLICATION_ID=$(createApplication)
     echo "Created application with id $APPLICATION_ID"
 
-    DEPLOYMENT_GROUP_ID=$(createDeploymentGroup "$CODE_DEPLOYER_PROFILE_ARN")
+    DEPLOYMENT_GROUP_ID=$(createDeploymentGroup)
     echo "Created Deployment group with id $DEPLOYMENT_GROUP_ID"
 
-    createBucket "$CODE_DEPLOYER_PROFILE_ARN"
+    createBucket
 }
 
 deleteAllResources(){
