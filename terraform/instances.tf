7b058a09c92ba98f06610bd081a7a6dc63c03e52
Andrew Gorton <a.gorton@kainos.com>
2015-12-15 12:18:20 +0000

    New instance for the xjson server

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -7,6 +7,19 @@ resource "aws_instance" "coreos-instances-tf" {
     vpc_security_group_ids = [ "${aws_security_group.coreos-tf.id}" ]
 
     tags {
-      Name = "coreos (instance ${count.index}) (tf)"
+      Name = "etcd #${count.index} (coreos) (tf)"
+    }
+}
+
+resource "aws_instance" "coreos-certificate-transparency-docker-tf" {
+    depends_on = ["aws_instance.coreos-instances-tf"]
+    ami = "ami-fd6ccd8e"
+    instance_type = "t1.micro"
+    user_data = "${file("certificate-transparency.yml")}"
+    subnet_id = "${aws_subnet.coreos-subnet-tf.id}"
+    vpc_security_group_ids = [ "${aws_security_group.coreos-tf.id}" ]
+
+    tags {
+      Name = "CT (coreos) (tf)"
     }
 }
