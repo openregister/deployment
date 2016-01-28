4de890f7db0b7360044898ae6f25dbd4b876f076
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-01-28 15:29:47 +0000

    assign IAM role to register instance
    This ensures that the instance can eg access necessary S3 buckets

--- aws/modules/register/instance.tf
+++ aws/modules/register/instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "register" {
   subnet_id = "${element(split(" ", var.subnet_ids), count.index)}"
   user_data = "${var.user_data}"
   security_groups = [ "${split(" ", var.security_group_ids)}" ]
+  iam_instance_profile = "${aws_iam_instance_profile.register_instance_profile.id}"
 
   tags = {
     Name = "${var.vpc_name}-${var.id}-${count.index +1}"
