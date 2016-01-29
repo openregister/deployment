9961dc8807f355345201b4bca0b1e395ea5665fe
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-01-29 15:46:58 +0000

    allow ELB to access instances over http

--- aws/modules/register/elb.tf
+++ aws/modules/register/elb.tf
@@ -14,6 +14,13 @@ resource "aws_security_group" "register_elb" {
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    security_groups = [ "${split(" ", var.security_group_ids)}" ]
+  }
 }
 
 resource "aws_elb" "register" {
