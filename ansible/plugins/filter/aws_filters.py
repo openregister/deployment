ecb236e4e924c9338a8aad420177e19eec32cbad
Slawomir Malyszek <slawomir.malyszek@digital.cabinet-office.gov.uk>
2016-03-09 14:17:35 +0000

    renamed aws filter custom plugin

--- ansible/plugins/filter/aws_filters.py
+++ ansible/plugins/filter/aws_filters.py
@@ -5,9 +5,14 @@ def normalize_ec2_tags(v=''):
   v = v.replace('-', '_')   # Replace hyphens with underscores
   return v
 
+def normalize_s3_bucket_name(v=''):
+  v= v.replace('.', '-')    # Replace dots with hyphens
+  return v
+
 class FilterModule(object):
   filter_map = {
-    'normalize_ec2_tags': normalize_ec2_tags
+    'normalize_ec2_tags': normalize_ec2_tags,
+    'normalize_s3_bucket_name': normalize_s3_bucket_name
   }
 
   def filters(self):
