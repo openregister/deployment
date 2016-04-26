c96980d3c837de6e6381634df4a52442999e4d15
michaelabroschomb <michaela.broschomb@digital.cabinet-office.gov.uk>
2016-04-26 15:47:33 +0100

    Add new fields and registers yaml files to config access policy

--- aws/modules/instance_policy/iam_policy_config_access.tf
+++ aws/modules/instance_policy/iam_policy_config_access.tf
@@ -14,7 +14,9 @@ resource "aws_iam_role_policy" "policy_config_access" {
         "arn:aws:s3:::openregister.${var.vpc_name}.config/${var.id}/mint/*",
         "arn:aws:s3:::openregister.${var.vpc_name}.config/${var.id}/presentation/*",
         "arn:aws:s3:::openregister.${var.vpc_name}.config/fields.yaml",
-        "arn:aws:s3:::openregister.${var.vpc_name}.config/registers.yaml"
+        "arn:aws:s3:::openregister.${var.vpc_name}.config/registers.yaml",
+        "arn:aws:s3:::openregister.${var.vpc_name}.config/new-fields.yaml",
+        "arn:aws:s3:::openregister.${var.vpc_name}.config/new-registers.yaml"
       ],
       "Effect": "Allow"
     }
