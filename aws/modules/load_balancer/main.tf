f0c2c7f9fc22eabdbdfa5656899a50c86dbe04af
Michaela Benyohai <michaelabenyohai@gds888.local>
2016-07-07 09:11:07 +0100

    Remove elb from name to ensure the name does not exceed string length limits

--- aws/modules/load_balancer/main.tf
+++ aws/modules/load_balancer/main.tf
@@ -2,7 +2,7 @@ resource "aws_elb" "load_balancer" {
   count = "${var.enabled}"
 
   # replace discovery with disco to ensure name does not exceed string length limit
-  name = "${replace(format("%s-%s-elb", replace(var.vpc_name,"discovery","disco"), var.id),"_","-")}"
+  name = "${replace(format("%s-%s", replace(var.vpc_name,"discovery","disco"), var.id),"_","-")}"
   subnets = [ "${split(" ", var.subnet_ids)}" ]
   security_groups = ["${aws_security_group.load_balancer.id}"]
 
