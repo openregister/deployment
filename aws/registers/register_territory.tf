47e96f8a77d5272d2cfd27910ff2fa67c4e2ac2b
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-08-02 15:22:34 +0100

    remove territory mint and presentation instances

--- aws/registers/register_territory.tf
+++ aws/registers/register_territory.tf
@@ -24,40 +24,6 @@ module "territory_openregister" {
   user_data = "${template_file.user_data.rendered}"
 }
 
-module "territory_presentation" {
-  source = "../modules/instance"
-  id = "territory"
-  role = "presentation_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
-
-  instance_count = "${lookup(var.instance_count, "territory")}"
-  iam_instance_profile = "${module.territory_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "territory_mint" {
-  source = "../modules/instance"
-  id = "territory"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "territory"))}"
-  iam_instance_profile = "${module.territory_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
 module "territory_elb" {
   source = "../modules/load_balancer"
   id = "territory"
@@ -73,19 +39,3 @@ module "territory_elb" {
   dns_zone_id = "${module.core.dns_zone_id}"
   certificate_arn = "${var.elb_certificate_arn}"
 }
-
-# module "territory_lb" {
-#   source = "../modules/load_balancer"
-#   id = "territory-new"
-#   enabled = "${signum(lookup(var.instance_count, "territory"))}"
-
-#   vpc_name = "${var.vpc_name}"
-#   vpc_id = "${module.core.vpc_id}"
-
-#   instance_ids = "${module.territory_openregister.instance_ids}"
-#   security_group_ids = "${module.openregister.security_group_id}"
-#   subnet_ids = "${module.core.public_subnet_ids}"
-
-#   dns_zone_id = "${module.core.dns_zone_id}"
-#   certificate_arn = "${var.elb_certificate_arn}"
-# }
