22184b49ef02e0ca09dbeae4aed73bf9be7f4587
Michaela Benyohai <michaelabenyohai@gds888.local>
2016-08-03 16:06:52 +0100

    Remove mint and presentation instances and replace with openregister instances for industry

--- aws/registers/register_industry.tf
+++ aws/registers/register_industry.tf
@@ -7,40 +7,6 @@ module "industry_policy" {
   vpc_id = "${module.core.vpc_id}"
 }
 
-module "industry_presentation" {
-  source = "../modules/instance"
-  id = "industry"
-  role = "presentation_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
-
-  instance_count = "${lookup(var.instance_count, "industry")}"
-  iam_instance_profile = "${module.industry_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "industry_mint" {
-  source = "../modules/instance"
-  id = "industry"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "industry"))}"
-  iam_instance_profile = "${module.industry_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
 module "industry_openregister" {
   source = "../modules/instance"
   id = "industry"
@@ -66,8 +32,8 @@ module "industry_elb" {
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
 
-  instance_ids = "${module.industry_presentation.instance_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
+  instance_ids = "${module.industry_openregister.instance_ids}"
+  security_group_ids = "${module.openregister.security_group_id}"
   subnet_ids = "${module.core.public_subnet_ids}"
 
   dns_zone_id = "${module.core.dns_zone_id}"
