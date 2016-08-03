6eae722e7fb38709534ee4977117f20a300e9748
Michaela Benyohai <michaelabenyohai@gds888.local>
2016-08-03 16:55:11 +0100

    Change location register to use new openregister instance

--- aws/registers/register_location.tf
+++ aws/registers/register_location.tf
@@ -7,40 +7,6 @@ module "location_policy" {
   vpc_id = "${module.core.vpc_id}"
 }
 
-module "location_presentation" {
-  source = "../modules/instance"
-  id = "location"
-  role = "presentation_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
-
-  instance_count = "${lookup(var.instance_count, "location")}"
-  iam_instance_profile = "${module.location_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "location_mint" {
-  source = "../modules/instance"
-  id = "location"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "location"))}"
-  iam_instance_profile = "${module.location_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
 module "location_openregister" {
   source = "../modules/instance"
   id = "location"
@@ -66,8 +32,8 @@ module "location_elb" {
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
 
-  instance_ids = "${module.location_presentation.instance_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
+  instance_ids = "${module.location_openregister.instance_ids}"
+  security_group_ids = "${module.openregister.security_group_id}"
   subnet_ids = "${module.core.public_subnet_ids}"
 
   dns_zone_id = "${module.core.dns_zone_id}"
