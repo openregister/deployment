9d8cc3409307f722cb1654f1a0357d58f9a1eab5
Michaela Benyohai <michaelabenyohai@gds888.local>
2016-08-04 14:08:36 +0100

    Change premises and food-premises-rating to use new openregister instance

--- aws/registers/register_premises.tf
+++ aws/registers/register_premises.tf
@@ -7,40 +7,6 @@ module "premises_policy" {
   vpc_id = "${module.core.vpc_id}"
 }
 
-module "premises_presentation" {
-  source = "../modules/instance"
-  id = "premises"
-  role = "presentation_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
-
-  instance_count = "${lookup(var.instance_count, "premises")}"
-  iam_instance_profile = "${module.premises_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "premises_mint" {
-  source = "../modules/instance"
-  id = "premises"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "premises"))}"
-  iam_instance_profile = "${module.premises_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
 module "premises_openregister" {
   source = "../modules/instance"
   id = "premises"
@@ -66,8 +32,8 @@ module "premises_elb" {
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
 
-  instance_ids = "${module.premises_presentation.instance_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
+  instance_ids = "${module.premises_openregister.instance_ids}"
+  security_group_ids = "${module.openregister.security_group_id}"
   subnet_ids = "${module.core.public_subnet_ids}"
 
   dns_zone_id = "${module.core.dns_zone_id}"
