30fce33045be95dde3ae5acea3a5dc46fe995674
robmckinnon <rob@movingflow>
2016-08-17 17:09:22 +0100

    Add register_school_authority.tf file

---
+++ aws/registers/register_school_authority.tf
@@ -0,0 +1,58 @@
+module "school-authority_policy" {
+  source = "../modules/instance_policy"
+  id = "school-authority"
+  enabled = "${signum(lookup(var.instance_count, "school-authority"))}"
+
+  vpc_name = "${var.vpc_name}"
+  vpc_id = "${module.core.vpc_id}"
+}
+
+module "school-authority_presentation" {
+  source = "../modules/instance"
+  id = "school-authority"
+  role = "presentation_app"
+
+  vpc_name = "${var.vpc_name}"
+  vpc_id = "${module.core.vpc_id}"
+
+  subnet_ids = "${module.presentation.subnet_ids}"
+  security_group_ids = "${module.presentation.security_group_id}"
+
+  instance_count = "${lookup(var.instance_count, "school-authority")}"
+  iam_instance_profile = "${module.school-authority_policy.profile_name}"
+
+  user_data = "${template_file.user_data.rendered}"
+}
+
+module "school-authority_mint" {
+  source = "../modules/instance"
+  id = "school-authority"
+  role = "mint_app"
+
+  vpc_name = "${var.vpc_name}"
+  vpc_id = "${module.core.vpc_id}"
+
+  subnet_ids = "${module.mint.subnet_ids}"
+  security_group_ids = "${module.mint.security_group_id}"
+
+  instance_count = "${signum(lookup(var.instance_count, "school-authority"))}"
+  iam_instance_profile = "${module.school-authority_policy.profile_name}"
+
+  user_data = "${template_file.user_data.rendered}"
+}
+
+module "school-authority_elb" {
+  source = "../modules/load_balancer"
+  id = "school-authority"
+  enabled = "${signum(lookup(var.instance_count, "school-authority"))}"
+
+  vpc_name = "${var.vpc_name}"
+  vpc_id = "${module.core.vpc_id}"
+
+  instance_ids = "${module.school-authority_presentation.instance_ids}"
+  security_group_ids = "${module.presentation.security_group_id}"
+  subnet_ids = "${module.core.public_subnet_ids}"
+
+  dns_zone_id = "${module.core.dns_zone_id}"
+  certificate_arn = "${var.elb_certificate_arn}"
+}
