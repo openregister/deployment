c8194fe7ca80dd49a62c5e433d4355507fed6e41
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-08-19 17:01:17 +0100

    recreate school-authority based on new template

--- aws/registers/register_school_authority.tf
+++ aws/registers/register_school_authority.tf
@@ -7,35 +7,18 @@ module "school-authority_policy" {
   vpc_id = "${module.core.vpc_id}"
 }
 
-module "school-authority_presentation" {
+module "school-authority_openregister" {
   source = "../modules/instance"
   id = "school-authority"
-  role = "presentation_app"
+  role = "openregister_app"
 
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
 
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
+  subnet_ids = "${module.openregister.subnet_ids}"
+  security_group_ids = "${module.openregister.security_group_id}"
 
   instance_count = "${lookup(var.instance_count, "school-authority")}"
-  iam_instance_profile = "${module.school-authority_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "school-authority_mint" {
-  source = "../modules/instance"
-  id = "school-authority"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "school-authority"))}"
   iam_instance_profile = "${module.school-authority_policy.profile_name}"
 
   user_data = "${template_file.user_data.rendered}"
