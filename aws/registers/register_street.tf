e170886999b79e9b8b1dffc3004a4200fb4a217c
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2016-08-30 11:32:03 +0100

    Street and local-authority registers should use openregister instances

--- aws/registers/register_street.tf
+++ aws/registers/register_street.tf
@@ -7,40 +7,6 @@ module "street_policy" {
   vpc_id = "${module.core.vpc_id}"
 }
 
-module "street_presentation" {
-  source = "../modules/instance"
-  id = "street"
-  role = "presentation_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.presentation.subnet_ids}"
-  security_group_ids = "${module.presentation.security_group_id}"
-
-  instance_count = "${lookup(var.instance_count, "street")}"
-  iam_instance_profile = "${module.street_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
-module "street_mint" {
-  source = "../modules/instance"
-  id = "street"
-  role = "mint_app"
-
-  vpc_name = "${var.vpc_name}"
-  vpc_id = "${module.core.vpc_id}"
-
-  subnet_ids = "${module.mint.subnet_ids}"
-  security_group_ids = "${module.mint.security_group_id}"
-
-  instance_count = "${signum(lookup(var.instance_count, "street"))}"
-  iam_instance_profile = "${module.street_policy.profile_name}"
-
-  user_data = "${template_file.user_data.rendered}"
-}
-
 module "street_openregister" {
   source = "../modules/instance"
   id = "street"
