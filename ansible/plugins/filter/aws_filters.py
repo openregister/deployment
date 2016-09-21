51acc038e04e31b3ea7ba449f848c7a2d18f4aff
Chris Duraj <christopherduraj@gmail.com>
2016-09-21 12:22:55 +0100

    Upgrade template to generate env tfvar file compatible with terraform ver >= 0.7

--- ansible/plugins/filter/aws_filters.py
+++ ansible/plugins/filter/aws_filters.py
@@ -9,10 +9,14 @@ def normalize_s3_bucket_name(v=''):
   v= v.replace('.', '-')    # Replace dots with hyphens
   return v
 
+def wrap_elements_in_quotes(list):
+  return ['"' + x + '"' for x in list]
+
 class FilterModule(object):
   filter_map = {
     'normalize_ec2_tags': normalize_ec2_tags,
-    'normalize_s3_bucket_name': normalize_s3_bucket_name
+    'normalize_s3_bucket_name': normalize_s3_bucket_name,
+    'wrap_elements_in_quotes': wrap_elements_in_quotes
   }
 
   def filters(self):
