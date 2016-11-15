117ed184d78038c9134173491d0e125324b9ea1a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-15 10:48:24 +0000

    document module/redirect variables

--- aws/modules/redirect/variables.tf
+++ aws/modules/redirect/variables.tf
@@ -1,5 +1,15 @@
-variable "from" {}
-variable "to" {}
-variable "certificate_arn" {}
-variable "dns_zone_id" {}
-variable "enabled" {}
+variable "from" {
+  description = "Domain name to redirect from"
+}
+variable "to" {
+  description = "Hostname (optionally with http:// or https:// scheme prefix) to redirect requests to"
+}
+variable "certificate_arn" {
+  description = "ARN of certificate to use for TLS support"
+}
+variable "dns_zone_id" {
+  description = "ID of Route 53 hosted zone to host DNS record for name in `from` variable"
+}
+variable "enabled" {
+  description = "Whether to enable redirect or not"
+}
