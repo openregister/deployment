17c2c2d00738f85ffe8aeeead00b309329c522e8
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-15 10:50:37 +0000

    always redirect to https://jobcentre
    Before this commit, these are the redirects we would have:
    
    http://job-centre -> https://job-centre (done by CloudFront)
    https://job-centre -> http://jobcentre (done by S3)
    
    It feels weird to upgrade to https and then downgrade to http.  So, this
    commit changes the behaviour to do this instead:
    
    http://job-centre -> https://job-centre (done by CloudFront)
    https://job-centre -> https://jobcentre (done by S3)

--- aws/registers/register_job_centre.tf
+++ aws/registers/register_job_centre.tf
@@ -4,7 +4,7 @@ module "job-centre_redirect" {
   enabled = "${signum(lookup(var.instance_count, "job-centre"))}"
 
   from = "job-centre.${var.vpc_name}.openregister.org"
-  to   = "jobcentre.${var.vpc_name}.openregister.org"
+  to   = "https://jobcentre.${var.vpc_name}.openregister.org"
   certificate_arn = "${var.cloudfront_certificate_arn}"
   dns_zone_id = "${module.core.dns_zone_id}"
 }
