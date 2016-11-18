2178b2583ed23ec4fe84a8daa2fd671371c16cc0
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-18 13:26:42 +0000

    move route 53 public names from old ELB to new

--- aws/modules/load_balancer/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "load_balancer" {
-  count = "${var.enabled}"
-  zone_id = "${var.dns_zone_id}"
-  name = "${var.id}.${var.vpc_name}.${var.dns_domain}"
-  type = "CNAME"
-  ttl = "${var.dns_ttl}"
-  records = [ "${aws_elb.load_balancer.dns_name}" ]
-}
