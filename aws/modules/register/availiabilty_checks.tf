d6dba0c382de6a84349fbe863a7b19f10cda00b5
Sam Crang <sam.crang@gmail.com>
2016-12-07 13:43:45 +0000

    Enable StatusCake alerts
    This configures the StatusCake tests to trigger notifications to the
    (manually configured) contact group.

--- aws/modules/register/availiabilty_checks.tf
+++ aws/modules/register/availiabilty_checks.tf
@@ -3,6 +3,7 @@ resource "statuscake_test" "status_check_home" {
   website_url = "https://${aws_route53_record.load_balancer.fqdn}"
   test_type = "HTTP"
   check_rate = 300
+  contact_id = 55280
   count = "${signum(var.enable_availability_checks * var.instance_count)}"
 }
 
@@ -11,5 +12,6 @@ resource "statuscake_test" "status_check_records" {
   website_url = "https://${aws_route53_record.load_balancer.fqdn}/records"
   test_type = "HTTP"
   check_rate = 300
+  contact_id = 55280
   count = "${signum(var.enable_availability_checks * var.instance_count)}"
 }
