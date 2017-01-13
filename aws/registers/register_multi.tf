4b9276c718aab320ec50b6364687f1b0c538a670
Sam Crang <sam.crang@gmail.com>
2017-01-13 11:36:06 +0000

    Upgrade to Amazon Linux 2016.09.1
    Amazon Linux has rolling releases:
    
    > The Amazon Linux AMI repository structure is configured to deliver a
    > continuous flow of updates that allow you to roll from one version of
    > the Amazon Linux AMI to the next.
    >
    > ...
    >
    > In other words, Amazon Linux AMIs are treated as snapshots in time, with
    > a repository and update structure that provides the latest packages that
    > we have built and pushed into the repository.
    Source: https://aws.amazon.com/amazon-linux-ami/faqs/
    
    This means that it should just mean we have less packages to upgrade
    when we spin up a new instance. However, I'll apply it just to to the
    multi-tenanted groups only as we'll be trashing the single-tenanted
    groups soon.

--- aws/registers/register_multi.tf
+++ aws/registers/register_multi.tf
@@ -3,6 +3,7 @@ module "multi" {
   id = "multi"
   instance_count = "${lookup(var.instance_count, "multi")}"
   instance_type = "t2.medium"
+  instance_ami = "ami-c51e3eb6"
 
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
