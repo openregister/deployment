549dd4663484e7180911e35c484b1c84360b6e92
Sam Crang <sam.crang@gmail.com>
2017-01-16 14:53:59 +0000

    Remove unused single-tenancy related configuration
    We don't need to explicitly describe any of the registers, apart from
    "address" and the special "job-centre" redirect.

--- aws/modules/register/availiabilty_checks.tf
+++ aws/modules/register/availiabilty_checks.tf
@@ -1,22 +1,4 @@
 resource "statuscake_test" "status_check_home" {
-  website_name = "${var.vpc_name} - ${var.id} - home"
-  website_url = "https://${aws_route53_record.load_balancer.fqdn}"
-  test_type = "HTTP"
-  check_rate = 60
-  contact_id = 55280
-  count = "${signum(var.enable_availability_checks * var.instance_count)}"
-}
-
-resource "statuscake_test" "status_check_records" {
-  website_name = "${var.vpc_name} - ${var.id} - records"
-  website_url = "https://${aws_route53_record.load_balancer.fqdn}/records"
-  test_type = "HTTP"
-  check_rate = 60
-  contact_id = 55280
-  count = "${signum(var.enable_availability_checks * var.instance_count)}"
-}
-
-resource "statuscake_test" "status_check_home_multi" {
   count = "${var.enable_availability_checks * length(var.registers)}"
   website_name = "${var.vpc_name} - ${element(var.registers, count.index)} - home"
   website_url = "https://${element(aws_route53_record.multi.*.fqdn, count.index)}"
@@ -25,7 +7,7 @@ resource "statuscake_test" "status_check_home_multi" {
   contact_id = 55280
 }
 
-resource "statuscake_test" "status_check_records_multi" {
+resource "statuscake_test" "status_check_records" {
   count = "${var.enable_availability_checks * length(var.registers)}"
   website_name = "${var.vpc_name} - ${element(var.registers, count.index)} - home"
   website_url = "https://${element(aws_route53_record.multi.*.fqdn, count.index)}/records"
--- aws/registers/variables.tf
+++ aws/registers/variables.tf
@@ -47,83 +47,10 @@ variable "openregister_database_class_instance" {
 variable "openregister_database_master_password" {}
 variable "openregister_database_apply_immediately" { default = false }
 
-/*
-
- === Register toggles ===
-
- This *instance_count* block defines default registers
-
- Setting a register to 0 will disable register (or remove if register was already provisioned)
-
- To toggle register(s) per environment use registers/environments/<name>.tfvars instead, e.g.:
-
- $ grep instance_count environments/alpha.tfvars
-
- > instance_count.school = 1
- > instance_count.street = 1
-
- will provision school and street with single EC2 instances for each register.
-
-*/
-
 variable "instance_count" {
   default = {
-    // Core registers
-    "datatype" = 1
-    "field" = 1
-    "public-body" = 0
-    "register" = 1
-
-    "academy-school-eng" = 0
     "address" = 0
-    "company" = 0
-    "country" = 0
-    "court" = 0
-    "denomination" = 0
-    "dental-practice" = 0
-    "diocese" = 0
-    "food-authority" = 0
-    "food-premises" = 0
-    "food-premises-rating" = 0
-    "food-premises-type" = 0
-    "government-domain" = 0
-    "government-organisation" = 0
-    "government-website" = 0
-    "gp-practice" = 0
-    "industry" = 0
-    "internal-drainage-board" = 0
     "job-centre" = 0
-    "jobcentre" = 0
-    "la-maintained-school-eng" = 0
-    "local-authority" = 0
-    "local-authority-eng" = 0
-    "local-authority-nir" = 0
-    "local-authority-sct" = 0
-    "local-authority-type" = 0
-    "local-authority-wls" = 0
-    "place" = 0
-    "police-force" = 0
-    "premises" = 0
-    "prison" = 0
-    "register-office" = 0
-    "religious-character" = 0
-    "school" = 0
-    "school-admissions-policy" = 0
-    "school-authority" = 0
-    "school-authority-eng" = 0
-    "school-eng" = 0
-    "school-federation" = 0
-    "school-gender" = 0
-    "school-phase" = 0
-    "school-tag" = 0
-    "school-trust" = 0
-    "school-type" = 0
-    "social-housing-provider" = 0
-    "street" = 0
-    "street-custodian" = 0
-    "territory" = 0
-    "uk" = 0
-
     "multi" = 0
   }
 }
