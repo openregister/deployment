9a5f397b2fea7171fc4182b6da99d42bac4e75ca
Sam Crang <sam.crang@gmail.com>
2017-01-17 10:30:43 +0000

    Use the latest Amazon Linux AMI
    https://github.com/openregister/deployment/commit/4b9276c718aab320ec50b6364687f1b0c538a670

--- aws/registers/register_group_basic.tf
+++ aws/registers/register_group_basic.tf
@@ -3,7 +3,6 @@ module "basic" {
   id = "basic"
   instance_count = "${lookup(var.group_instance_count, "basic", 0)}"
   instance_type = "${lookup(var.group_instance_type, "basic", "t2.medium")}"
-  instance_ami = "ami-c51e3eb6"
 
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
--- aws/registers/register_group_multi.tf
+++ aws/registers/register_group_multi.tf
@@ -3,7 +3,6 @@ module "multi" {
   id = "multi"
   instance_count = "${lookup(var.group_instance_count, "multi", 0)}"
   instance_type = "${lookup(var.group_instance_type, "multi", "t2.medium")}"
-  instance_ami = "ami-c51e3eb6"
 
   vpc_name = "${var.vpc_name}"
   vpc_id = "${module.core.vpc_id}"
