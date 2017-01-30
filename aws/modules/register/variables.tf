dc1904a5b14506ed7bdd0633cee85a0670e5222b
Sam Crang <sam.crang@gmail.com>
2017-01-30 11:31:45 +0000

    Add comments to describe module variable usage

--- aws/modules/register/variables.tf
+++ aws/modules/register/variables.tf
@@ -1,9 +1,24 @@
-variable name {}
-variable environment {}
-variable enabled {}
-variable dns_zone_id {}
-variable load_balancer {
-  type = "map"
+variable name {
+  description = "name of the register"
 }
 
-variable enable_availability_checks {}
+variable environment {
+  description = "the environment which this register is in"
+}
+
+variable enabled {
+  description = "is this register enabled in this environment"
+}
+
+variable dns_zone_id {
+  description = "the Route 53 DNS zone to create these records in"
+}
+
+variable load_balancer {
+  type = "map"
+  description = "output from the register_group module"
+}
+
+variable enable_availability_checks {
+  description = "should availability checks be enabled for this register"
+}
