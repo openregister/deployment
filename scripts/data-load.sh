c50fb79e0a4c7d086c26ac73d1c721ddfdc29b50
robmckinnon <rob@movingflow>
2017-02-03 14:38:49 +0000

    Made minor suggested improvements
    Add set -e to script.
    
    Assign nice var names for arguments passed to script.
    
    Change to git pull --rebase on data repo.
    
    Clone credentials to ~/.register-pass
    
    Remove sed as serializer output is fixed now.

--- scripts/data-load.sh
+++ scripts/data-load.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
+set -e
 
-echo "register: $1"
-echo "phase: $2"
+echo "usage: ./scripts/data-load.sh [register] [phase]"
+echo ""
+
+REGISTER=$1
+PHASE=$2
+echo "register: $REGISTER"
+echo "phase: $PHASE"
 
 echo ""
-echo "Downloading $2 field-records.json"
+echo "Downloading $PHASE field-records.json"
 rm -f field-records.json
-curl -sS https://field.$2.openregister.org/records.json > field-records.json
+curl -sS https://field.$PHASE.openregister.org/records.json > field-records.json
 
 echo ""
-[[ -e ../$1-data ]] || echo "cloning $1-data repo"
-[[ -e ../$1-data ]] || (cd .. && git clone git@github.com:openregister/$1-data.git)
-echo "../$1-data repo: checkout master && pull"
-cd ../$1-data && git checkout master && git fetch origin && git pull
+[[ -e ../$REGISTER-data ]] || echo "cloning $REGISTER-data repo"
+[[ -e ../$REGISTER-data ]] || (cd .. && git clone git@github.com:openregister/$REGISTER-data.git)
+echo "../$REGISTER-data repo: checkout master && pull"
+cd ../$REGISTER-data && git checkout master && git pull --rebase
 
 echo ""
-echo "Get credentials for $2 $1"
-[[ -e ../credentials ]] || echo "cloning credentials repo"
-[[ -e ../credentials ]] || (cd .. && git clone git@github.com:openregister/credentials.git)
-cd ../credentials && PASSWORD=`PASSWORD_STORE_DIR=~/.registers-pass pass $2/app/mint/$1`
+echo "Get credentials for $PHASE $REGISTER"
+[[ -e ~/.registers-pass ]] || echo "cloning credentials repo to ~/.registers-pass"
+[[ -e ~/.registers-pass ]] || (cd ~ && git clone git@github.com:openregister/credentials.git .registers-pass)
+PASSWORD=`PASSWORD_STORE_DIR=~/.registers-pass pass $PHASE/app/mint/$REGISTER`
 
 echo ""
-echo "Serialize tsv to $1.rsf"
+echo "Serialize tsv to $REGISTER.rsf"
 cd ../deployment
-if [[ -e ../$1-data/data/$2/$1/$1es.tsv ]]; then
-  serializer tsv field-records.json ../$1-data/data/$2/$1/$1es.tsv $1 > $1.rsf
-elif [[ -e ../$1-data/data/$2/$1/$1s.tsv ]]; then
-  serializer tsv field-records.json ../$1-data/data/$2/$1/$1s.tsv $1 > $1.rsf
-elif [[ -e ../$1-data/data/$2/$1/$1.tsv ]]; then
-  serializer tsv field-records.json ../$1-data/data/$2/$1/$1.tsv  $1 > $1.rsf
+TSVFILE="../$REGISTER-data/data/$PHASE/$REGISTER/$REGISTER"
+if [[ -e $TSVFILE"es.tsv" ]]; then
+  serializer tsv field-records.json $TSVFILE"es.tsv" $REGISTER > $REGISTER.rsf
+elif [[ -e $TSVFILE"s.tsv" ]]; then
+  serializer tsv field-records.json $TSVFILE"s.tsv" $REGISTER > $REGISTER.rsf
+elif [[ -e $TSVFILE".tsv" ]]; then
+  serializer tsv field-records.json $TSVFILE".tsv" $REGISTER > $REGISTER.rsf
 fi
 
 echo ""
-echo -n "Delete existing $1 data from $2 register? (y/n)? "
+echo -n "Delete existing $REGISTER data from $PHASE register? (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ; then
     echo ""
-    echo "Deleting $1 data from $2"
-    curl -X DELETE -u openregister:$PASSWORD https://$1.$2.openregister.org/delete-register-data
+    echo "Deleting $REGISTER data from $PHASE"
+    curl -X DELETE -u openregister:$PASSWORD https://$REGISTER.$PHASE.openregister.org/delete-register-data
 fi
 
 echo ""
-echo -n "Load $1 data into $2 register? (y/n)? "
+echo -n "Load $REGISTER data into $PHASE register? (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ; then
   echo ""
-  echo "Loading $1 data to $2"
-  echo `cat $1.rsf | sed 's/sha256/sha-256/' | curl -X POST -u openregister:$PASSWORD --data-binary @- https://$1.$2.openregister.org/load-rsf --header "Content-Type:application/uk-gov-rsf"`
+  echo "Loading $REGISTER data to $PHASE"
+  echo `cat $REGISTER.rsf | curl -X POST -u openregister:$PASSWORD --data-binary @- https://$REGISTER.$PHASE.openregister.org/load-rsf --header "Content-Type:application/uk-gov-rsf"`
 fi
 
 echo ""
 echo "Removing temporary files"
-rm $1.rsf
+rm $REGISTER.rsf
 rm field-records.json
 
 echo ""
