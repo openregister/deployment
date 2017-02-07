07e39884c17118e1881e3317aa210fd5ece9558f
Sam Crang <sam.crang@gmail.com>
2017-02-07 11:28:38 +0000

    Upgrade CloudTrail deployment to use Terraform 0.8.5
    We're relying on some upstream Terraform modules that have changed in a
    non-backwards compatible way since they were deployed. At the moment I
    don't want to change any behaviour as there's a card to look at this in
    the backlog.

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -3,27 +3,27 @@ provider "aws" {
 }
 
 module "aws-security-alarms" {
-  source                      = "github.com/alphagov/aws-security-alarms//terraform"
+  source                      = "git::github.com/alphagov/aws-security-alarms.git//terraform?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
   cloudtrail_s3_bucket_name   = "cloudtrail-logs-openregister"
   cloudtrail_s3_bucket_prefix = "prod"
 }
 
 module "unexpected-ip-access" {
-  source               = "github.com/alphagov/aws-security-alarms//terraform/alarms/unexpected_ip_access"
+  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/unexpected_ip_access?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
   environment_name     = "test"
   cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
   alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
 }
 
 module "unauthorized-activity" {
-  source               = "github.com/alphagov/aws-security-alarms//terraform/alarms/unauthorized_activity"
+  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/unauthorized_activity?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
   environment_name     = "test"
   cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
   alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
 }
 
 module "root-activity" {
-  source               = "github.com/alphagov/aws-security-alarms//terraform/alarms/root_activity"
+  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/root_activity?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
   environment_name     = "test"
   cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
   alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
