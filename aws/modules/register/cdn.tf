04d17bea7d5116c7c93e7b927d4c49fa5ad2ec0c
Sam Crang <sam.crang@gmail.com>
2017-02-09 16:56:43 +0000

    Use only the latest TLS version to connect to the origin
    This should not affect the viewer TLS configuration which is still TLSv1
    as a minimum

--- aws/modules/register/cdn.tf
+++ aws/modules/register/cdn.tf
@@ -39,7 +39,7 @@ resource "aws_cloudfront_distribution" "distribution" {
       http_port = 80
       https_port = 443
       origin_protocol_policy = "https-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_ssl_protocols = ["TLSv1.2"]
     }
   }
 
