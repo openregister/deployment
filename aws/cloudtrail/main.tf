728d2546ce0de37f646e4d58e83386a81fa04ea7
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2017-02-16 17:43:51 +0000

    disable alarms
    these aren't really configured right

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -7,24 +7,3 @@ module "aws-security-alarms" {
   cloudtrail_s3_bucket_name   = "cloudtrail-logs-openregister"
   cloudtrail_s3_bucket_prefix = "prod"
 }
-
-module "unexpected-ip-access" {
-  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/unexpected_ip_access?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
-  environment_name     = "test"
-  cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
-  alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
-}
-
-module "unauthorized-activity" {
-  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/unauthorized_activity?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
-  environment_name     = "test"
-  cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
-  alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
-}
-
-module "root-activity" {
-  source               = "git::github.com/alphagov/aws-security-alarms.git//terraform/alarms/root_activity?ref=89c8f28c16b91f06cc4aa5765786614653cc9d57"
-  environment_name     = "test"
-  cloudtrail_log_group = "${module.aws-security-alarms.cloudtrail_log_group}"
-  alarm_actions        = ["${module.aws-security-alarms.security_alerts_topic}"]
-}
