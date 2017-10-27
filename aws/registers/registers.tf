d486ca01bd3b5ba42165c5c912e5b9772ac41f1c
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-10-27 14:42:22 +0100

    add charity and charity-class

--- aws/registers/registers.tf
+++ aws/registers/registers.tf
@@ -32,6 +32,40 @@ module "academy-school-eng_register" {
   pingdom_contact_ids = "${var.pingdom_contact_ids}"
 }
 
+module "charity" {
+  source = "../modules/register"
+  enabled = "${lookup(var.enabled_registers, "charity", false)}"
+
+  name = "charity"
+  environment = "${var.environment_name}"
+  dns_zone_id = "${module.core.dns_zone_id}"
+
+  enable_availability_checks = "${var.enable_availability_checks}"
+  cdn_configuration = "${var.cdn_configuration}"
+  cdn_s3_origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  cdn_dns_zone_id = "${module.core.cdn_dns_zone_id}"
+  paas_cdn_domain_name = "${aws_cloudfront_distribution.paas_cdn.domain_name}"
+  paas_cdn_hosted_zone_id = "${aws_cloudfront_distribution.paas_cdn.hosted_zone_id}"
+  pingdom_contact_ids = "${var.pingdom_contact_ids}"
+}
+
+module "charity-class" {
+  source = "../modules/register"
+  enabled = "${lookup(var.enabled_registers, "charity-class", false)}"
+
+  name = "charity-class"
+  environment = "${var.environment_name}"
+  dns_zone_id = "${module.core.dns_zone_id}"
+
+  enable_availability_checks = "${var.enable_availability_checks}"
+  cdn_configuration = "${var.cdn_configuration}"
+  cdn_s3_origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  cdn_dns_zone_id = "${module.core.cdn_dns_zone_id}"
+  paas_cdn_domain_name = "${aws_cloudfront_distribution.paas_cdn.domain_name}"
+  paas_cdn_hosted_zone_id = "${aws_cloudfront_distribution.paas_cdn.hosted_zone_id}"
+  pingdom_contact_ids = "${var.pingdom_contact_ids}"
+}
+
 module "clinical-commissioning-group_register" {
   source = "../modules/register"
   enabled = "${lookup(var.enabled_registers, "clinical-commissioning-group", false)}"
