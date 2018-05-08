2127034737a7456d314a11221fad2bba34567410
michaelabenyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2018-05-08 11:08:39 +0100

    Create new alpha registers
    - approved-open-standard
    - approved-open-standard-guidance

--- aws/registers/registers.tf
+++ aws/registers/registers.tf
@@ -16,6 +16,42 @@ module "allergen_register" {
   api_key_to_cloudfront_logs_version_number = "${var.api_key_to_cloudfront_logs_version_number}"
 }
 
+module "approved-open-standard_register" {
+  source = "../modules/register"
+  enabled = "${lookup(var.enabled_registers, "approved-open-standard", false)}"
+
+  name = "approved-open-standard"
+  environment = "${var.environment_name}"
+  dns_zone_id = "${module.core.dns_zone_id}"
+
+  enable_availability_checks = "${var.enable_availability_checks}"
+  cdn_configuration = "${var.cdn_configuration}"
+  cdn_s3_origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  cdn_dns_zone_id = "${module.core.cdn_dns_zone_id}"
+  paas_cdn_domain_name = "${aws_cloudfront_distribution.paas_cdn.domain_name}"
+  paas_cdn_hosted_zone_id = "${aws_cloudfront_distribution.paas_cdn.hosted_zone_id}"
+  pingdom_contact_ids = "${var.pingdom_contact_ids}"
+  api_key_to_cloudfront_logs_version_number = "${var.api_key_to_cloudfront_logs_version_number}"
+}
+
+module "approved-open-standard-guidance_register" {
+  source = "../modules/register"
+  enabled = "${lookup(var.enabled_registers, "approved-open-standard-guidance", false)}"
+
+  name = "approved-open-standard-guidance"
+  environment = "${var.environment_name}"
+  dns_zone_id = "${module.core.dns_zone_id}"
+
+  enable_availability_checks = "${var.enable_availability_checks}"
+  cdn_configuration = "${var.cdn_configuration}"
+  cdn_s3_origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  cdn_dns_zone_id = "${module.core.cdn_dns_zone_id}"
+  paas_cdn_domain_name = "${aws_cloudfront_distribution.paas_cdn.domain_name}"
+  paas_cdn_hosted_zone_id = "${aws_cloudfront_distribution.paas_cdn.hosted_zone_id}"
+  pingdom_contact_ids = "${var.pingdom_contact_ids}"
+  api_key_to_cloudfront_logs_version_number = "${var.api_key_to_cloudfront_logs_version_number}"
+}
+
 module "country_register" {
   source = "../modules/register"
   enabled = "${lookup(var.enabled_registers, "country", false)}"
