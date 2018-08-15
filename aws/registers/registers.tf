0005130755f41359e34fa1870286cbfec25615b9
Mat Moore <matmoore@users.noreply.github.com>
2018-08-15 11:08:42 +0100

    Add terraform for clinical commissioning group

--- aws/registers/registers.tf
+++ aws/registers/registers.tf
@@ -52,6 +52,24 @@ module "approved-open-standard-guidance_register" {
   api_key_to_cloudfront_logs_version_number = "${var.api_key_to_cloudfront_logs_version_number}"
 }
 
+module "clinical-commissioning-group" {
+  source = "../modules/register"
+  enabled = "${lookup(var.enabled_registers, "clinical-commissioning-group", false)}"
+
+  name = "clinical-commissioning-group"
+  environment = "${var.environment_name}"
+  dns_zone_id = "${module.core.dns_zone_id}"
+
+  enable_availability_checks = "${var.enable_availability_checks}"
+  cdn_configuration = "${var.cdn_configuration}"
+  cdn_s3_origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  cdn_dns_zone_id = "${module.core.cdn_dns_zone_id}"
+  paas_cdn_domain_name = "${aws_cloudfront_distribution.paas_cdn.domain_name}"
+  paas_cdn_hosted_zone_id = "${aws_cloudfront_distribution.paas_cdn.hosted_zone_id}"
+  pingdom_contact_ids = "${var.pingdom_contact_ids}"
+  api_key_to_cloudfront_logs_version_number = "${var.api_key_to_cloudfront_logs_version_number}"
+}
+
 module "country_register" {
   source = "../modules/register"
   enabled = "${lookup(var.enabled_registers, "country", false)}"
