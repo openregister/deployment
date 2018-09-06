70b4059bd11179987ea295768c5b6fa54fdf8550
Mat Moore <matmoore@users.noreply.github.com>
2018-09-06 12:42:36 +0100

    Write a script to check whether github reflects reality
    Check that register register source data (and friendly names we store in
    github) are consistent with the individual registers.

--- scripts/rsflib/parser.py
+++ scripts/rsflib/parser.py
@@ -1,10 +1,51 @@
 import json
 import re
+import warnings
 from rsflib.rsf import RsfCommand, RsfAddItem, RsfAppendEntry
 
 
 ITEM_REGEX = re.compile(r'^add-item\t({[^\t]*})$')
 ENTRY_REGEX = re.compile(r'^append-entry\t([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)$')
+
+
+class RsfFile:
+    def __init__(self, register_name, lines):
+        self.lines = lines
+        self.commands = []
+        self.register_name = register_name
+
+        for line in lines:
+            parsed_line = parse_rsf_line(line.decode('utf8'))
+            if parsed_line is not None:
+                self.commands.append(parsed_line)
+
+        self.friendly_name, self.register_record = self._find_metadata()
+
+    def _find_metadata(self):
+        """
+        Find the register-name and register:{register-id} records
+        """
+        items = {}
+        register_entry = None
+        register_name_entry = None
+
+        for rsf in self.commands:
+            if rsf.command == RsfCommand.ADD_ITEM:
+                items[rsf.item_hash()] = rsf
+
+            elif rsf.command == RsfCommand.APPEND_ENTRY:
+                if rsf.key == f'register:{self.register_name}':
+                    register_entry = rsf.item_hash
+                elif rsf.key == f'register-name':
+                    register_name_entry = rsf.item_hash
+
+        register_name_item = items.get(register_name_entry)
+        register_record_item = items.get(register_entry)
+
+        return (
+            None if register_name_item is None else register_name_item.item['register-name'],
+            None if register_record_item is None else register_record_item.item
+        )
 
 
 def parse_rsf_line(line):
@@ -32,6 +73,8 @@ def find_register_item(register_name, lines):
     """
     Find an item with key register:{register_name}
     """
+    warnings.warn("deprecated - use RsfFile class instead", DeprecationWarning)
+
     items = {}
     most_recent_item = None
 
