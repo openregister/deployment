41254ae0f6e4924672b9238a416fc93bd83f7b07
Mat Moore <matmoore@users.noreply.github.com>
2018-09-19 16:52:03 +0100

    Reformat file
    This file uses a mix of tabs and spaces. This converts everything to tabs
    which seems to be the most common style

--- aws/lambda/node/cloudfront-logs-api-key-to-google-analytics/lambda.js
+++ aws/lambda/node/cloudfront-logs-api-key-to-google-analytics/lambda.js
@@ -10,66 +10,66 @@ const s4 = () => {
 
 const guid = () => {
 	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+		s4() + '-' + s4() + s4() + s4();
 };
 
 exports.handler = function (input, context, callback) {
 	const payload = Buffer.from(input.awslogs.data, 'base64');
-  zlib.gunzip(payload, (e, result) => {
-  	if (e) {
-      callback(e);
-  	} else {
-  		result = JSON.parse(result.toString('ascii'));
-  		const eventCount = result.logEvents.length;
-      result.logEvents.forEach(logEvent => {
-      	const event = logEvent.message.split('\t').slice(-1)[0];
-        console.log(event);
-        const requestData = JSON.parse(event);
-        console.log(requestData);
+	zlib.gunzip(payload, (e, result) => {
+		if (e) {
+			callback(e);
+		} else {
+			result = JSON.parse(result.toString('ascii'));
+			const eventCount = result.logEvents.length;
+			result.logEvents.forEach(logEvent => {
+				const event = logEvent.message.split('\t').slice(-1)[0];
+				console.log(event);
+				const requestData = JSON.parse(event);
+				console.log(requestData);
 
-        const envTid = process.env.TID;
+				const envTid = process.env.TID;
 
-        const data = querystring.stringify({
-        	v: 1,
-        	t: 'pageview',
-        	tid: envTid,
-        	cid: guid(),
-        	aip: 1,
-        	ni: 1,
-        	dl: requestData.endpoint,
-        	cd2: requestData.apikey,
+				const data = querystring.stringify({
+					v: 1,
+					t: 'pageview',
+					tid: envTid,
+					cid: guid(),
+					aip: 1,
+					ni: 1,
+					dl: requestData.endpoint,
+					cd2: requestData.apikey,
 					cd5: requestData.hittype,
 					ua: requestData.useragent
-        });
+				});
 
-        const options = {
-        	host: 'www.google-analytics.com',
-        	hostname: 'www.google-analytics.com',
-        	port: 443,
-        	path: '/collect',
-        	method: 'POST',
-        	headers: {
-        		'Content-Type': 'application/x-www-form-urlencoded',
-        		'Content-Length': data.length
-        	}
-        };
+				const options = {
+					host: 'www.google-analytics.com',
+					hostname: 'www.google-analytics.com',
+					port: 443,
+					path: '/collect',
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+						'Content-Length': data.length
+					}
+				};
 
-        const request = https.request(options, res => {
-          console.log('statusCode:', res.statusCode);
+				const request = https.request(options, res => {
+					console.log('statusCode:', res.statusCode);
 
-          res.on('error', err => {
-            callback(err);
-          });
-        });
+					res.on('error', err => {
+						callback(err);
+					});
+				});
 
-        request.on('error', err => {
-          callback(err);
-        });
+				request.on('error', err => {
+					callback(err);
+				});
 
-        request.write(data);
-        request.end();
-      });
-      callback(null, `finished sending ${eventCount} event(s)`);
-  	}
-  });
+				request.write(data);
+				request.end();
+			});
+			callback(null, `finished sending ${eventCount} event(s)`);
+		}
+	});
 };
