42c26c97a87df296e92e576fe16d8884ae9b5888
Gideon Goldberg <gidsgoldberg@gmail.com>
2019-02-11 11:30:02 +0000

    remove standard dev dependency remove standard devDependency
    Fix standard lint errors in scripts

--- aws/lambda/node/cache-invalidator/lambda.js
+++ aws/lambda/node/cache-invalidator/lambda.js
@@ -1,70 +1,70 @@
-const zlib = require('zlib');
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved
+const zlib = require('zlib')
+const AWS = require('aws-sdk') // eslint-disable-line import/no-unresolved
 
-const cloudfront = new AWS.CloudFront();
+const cloudfront = new AWS.CloudFront()
 
 exports.handler = async event => {
-	// Log event
-	// console.log(JSON.stringify(event))
-	const payload = Buffer.from(event.awslogs.data, 'base64');
-	const parsed = JSON.parse(zlib.gunzipSync(payload).toString('utf8'));
-	console.log('Decoded payload:', JSON.stringify(parsed));
+  // Log event
+  // console.log(JSON.stringify(event))
+  const payload = Buffer.from(event.awslogs.data, 'base64')
+  const parsed = JSON.parse(zlib.gunzipSync(payload).toString('utf8'))
+  console.log('Decoded payload:', JSON.stringify(parsed))
 
-	const logEvent = parsed.logEvents[0];
-	const {message} = logEvent;
-	const messageParts = message.split('\t');
-	const messageObject = messageParts[2];
-	const parsedMessageObject = JSON.parse(messageObject);
-	console.log(parsedMessageObject);
-	const {distributionId} = parsedMessageObject;
-	const callerReference = 'cache-invalidator-lambda-' + new Date().toISOString();
-	const {host} = parsedMessageObject;
+  const logEvent = parsed.logEvents[0]
+  const {message} = logEvent
+  const messageParts = message.split('\t')
+  const messageObject = messageParts[2]
+  const parsedMessageObject = JSON.parse(messageObject)
+  console.log(parsedMessageObject)
+  const {distributionId} = parsedMessageObject
+  const callerReference = 'cache-invalidator-lambda-' + new Date().toISOString()
+  const {host} = parsedMessageObject
 
-	const getDistributionId = async () => {
-		const isBeta = host.split('.')[1] === 'beta';
+  const getDistributionId = async () => {
+    const isBeta = host.split('.')[1] === 'beta'
 
-		if (isBeta) {
-			let distributions;
-			try {
-				distributions = await cloudfront.listDistributions({MaxItems: '1000'}).promise();
-			} catch (error) {
-				throw new Error(`List Distributions failed: ${error}`);
-			}
-			const registerGovUkDomain = host.replace('beta.openregister.org', 'register.gov.uk');
-			const registerGovUkDistribution = distributions.Items.find(item => item.Aliases.Items.includes(registerGovUkDomain));
-			if (registerGovUkDistribution && registerGovUkDistribution.Id) {
-				return (registerGovUkDistribution.Id);
-			}
-			throw new Error('matching distribution not found');
-		} else {
-			return distributionId;
-		}
-	};
+    if (isBeta) {
+      let distributions
+      try {
+        distributions = await cloudfront.listDistributions({MaxItems: '1000'}).promise()
+      } catch (error) {
+        throw new Error(`List Distributions failed: ${error}`)
+      }
+      const registerGovUkDomain = host.replace('beta.openregister.org', 'register.gov.uk')
+      const registerGovUkDistribution = distributions.Items.find(item => item.Aliases.Items.includes(registerGovUkDomain))
+      if (registerGovUkDistribution && registerGovUkDistribution.Id) {
+        return (registerGovUkDistribution.Id)
+      }
+      throw new Error('matching distribution not found')
+    } else {
+      return distributionId
+    }
+  }
 
-	const distributionIdToInvalidate = await getDistributionId();
+  const distributionIdToInvalidate = await getDistributionId()
 
-	const params = {
-		DistributionId: distributionIdToInvalidate, /* Required */
-		InvalidationBatch: { /* Required */
-			CallerReference: callerReference, /* Required */
-			Paths: { /* Required */
-				Quantity: 9, /* Required */
-				Items: [
-					'/download-register',
-					'/',
-					'/records',
-					'/records*',
-					'/register*',
-					'/entries',
-					'/entries*',
-					'/download-rsf*',
-					'/proof/register/merkle:sha-256'
-				]
-			}
-		}
-	};
+  const params = {
+    DistributionId: distributionIdToInvalidate, /* Required */
+    InvalidationBatch: { /* Required */
+      CallerReference: callerReference, /* Required */
+      Paths: { /* Required */
+        Quantity: 9, /* Required */
+        Items: [
+          '/download-register',
+          '/',
+          '/records',
+          '/records*',
+          '/register*',
+          '/entries',
+          '/entries*',
+          '/download-rsf*',
+          '/proof/register/merkle:sha-256'
+        ]
+      }
+    }
+  }
 
-	console.log('Params:', JSON.stringify(params));
+  console.log('Params:', JSON.stringify(params))
 
-	return cloudfront.createInvalidation(params).promise();
-};
+  return cloudfront.createInvalidation(params).promise()
+}
